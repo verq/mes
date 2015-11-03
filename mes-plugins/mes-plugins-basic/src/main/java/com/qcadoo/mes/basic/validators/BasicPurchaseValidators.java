cda4d4f5f9165c11d99a560c9af308ce4b47dbc6
weronika <wmsikorska@gmail.com>
2015-11-03 18:02:24 +0100

    Added validations to price and quantity

---
+++ mes-plugins/mes-plugins-basic/src/main/java/com/qcadoo/mes/basic/validators/BasicPurchaseValidators.java
@@ -0,0 +1,80 @@
+/**
+ * ***************************************************************************
+ * Copyright (c) 2010 Qcadoo Limited
+ * Project: Qcadoo MES
+ * Version: 1.4
+ *
+ * This file is part of Qcadoo.
+ *
+ * Qcadoo is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * ***************************************************************************
+ */
+package com.qcadoo.mes.basic.validators;
+
+import com.qcadoo.mes.basic.constants.BasicPurchaseFields;
+import com.qcadoo.model.api.DataDefinition;
+import com.qcadoo.model.api.Entity;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class BasicPurchaseValidators {
+
+    public boolean validatesWith(final DataDefinition basicPurchaseDD, final Entity basicPurchase) {
+        return checkValueOfQuantity(basicPurchaseDD, basicPurchase) && checkValueOfPrice(basicPurchaseDD, basicPurchase);
+    }
+
+    private boolean checkValueOfQuantity(final DataDefinition basicPurchaseDD, final Entity basicPurchase) {
+        BigDecimal quantity = basicPurchase.getDecimalField(BasicPurchaseFields.QUANTITY);
+
+        if (quantity == null) {
+            return true;
+        }
+
+        if ((quantity != null) && (quantity.compareTo(BigDecimal.ZERO) <= 0)) {
+            basicPurchase.addError(basicPurchaseDD.getField(BasicPurchaseFields.QUANTITY),
+                    "basicPurchase.basicPurchase.quantity.error.lowerOrEqualZero");
+        }
+
+        if (!basicPurchase.getGlobalErrors().isEmpty() || !basicPurchase.getErrors().isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean checkValueOfPrice(final DataDefinition basicPurchaseDD, final Entity basicPurchase) {
+        BigDecimal price = basicPurchase.getDecimalField(BasicPurchaseFields.PRICE);
+
+        if (price == null) {
+            return true;
+        }
+
+        if ((price != null) && (price.compareTo(BigDecimal.ZERO) <= 0)) {
+            basicPurchase.addError(basicPurchaseDD.getField(BasicPurchaseFields.PRICE),
+                    "basicPurchase.basicPurchase.price.error.lowerOrEqualZero");
+        }
+
+        if (!basicPurchase.getGlobalErrors().isEmpty() || !basicPurchase.getErrors().isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+}
